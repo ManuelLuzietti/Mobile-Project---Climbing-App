117fb1ac0d8c8dfdceb81780b08f4cc2e5f62959
ManuelLuzietti <manuel.luzietti@studio.unibo.it>
2023-01-04 12:19:04 +0100

    now the app takes the img from internet only the first time then it remains saved on the phone

--- ClimbingApp/app/src/main/java/com/example/climbingapp/BoulderViewFragment.java
+++ ClimbingApp/app/src/main/java/com/example/climbingapp/BoulderViewFragment.java
@@ -1,6 +1,7 @@
 package com.example.climbingapp;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -24,14 +25,23 @@ import com.google.android.material.navigation.NavigationBarView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class BoulderViewFragment extends Fragment {
 
-    private SelectedBoulderViewModel model ;
+    private SelectedBoulderViewModel model;
     private InternetManager internetManager;
     private boolean mIsCreated;
+
     public BoulderViewFragment() {
         // Required empty public constructor
     }
@@ -40,7 +50,7 @@ public class BoulderViewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getActivity()!=null){
+        if (getActivity() != null) {
             model = new ViewModelProvider(getActivity()).get(SelectedBoulderViewModel.class);
         }
     }
@@ -62,35 +72,35 @@ public class BoulderViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_boulder_view, container, false);
-        internetManager = new InternetManager(getActivity(),view);
-        ((NavigationBarView)view.findViewById(R.id.bottomnavview_boulderview)).setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
+        View view = inflater.inflate(R.layout.fragment_boulder_view, container, false);
+        internetManager = new InternetManager(getActivity(), view);
+        ((NavigationBarView) view.findViewById(R.id.bottomnavview_boulderview)).setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
                 case R.id.bottomnav_add:
-                    try{
+                    try {
                         NavHostFragment.findNavController(FragmentManager.findFragment(view))
                                 .navigate(R.id.action_boulderViewFragment_to_addViewFragment);
-                    } catch (IllegalStateException is){
+                    } catch (IllegalStateException is) {
                         if (getActivity() != null) {
-                            Utils.insertFragment((AppCompatActivity) getActivity(),new AddViewFragment(),getClass().getSimpleName(),R.id.nav_host_fragment_menu);
+                            Utils.insertFragment((AppCompatActivity) getActivity(), new AddViewFragment(), getClass().getSimpleName(), R.id.nav_host_fragment_menu);
                         }
                     }
                     break;
                 case R.id.bottomnav_comments:
-                    try{
+                    try {
                         NavHostFragment.findNavController(FragmentManager.findFragment(view)).navigate(R.id.action_boulderViewFragment_to_commentsBoulderViewFragment);
-                    } catch (IllegalStateException is){
+                    } catch (IllegalStateException is) {
                         if (getActivity() != null) {
-                            Utils.insertFragment((AppCompatActivity) getActivity(),new CommentsBoulderViewFragment(),getClass().getSimpleName(),R.id.nav_host_fragment_menu);
+                            Utils.insertFragment((AppCompatActivity) getActivity(), new CommentsBoulderViewFragment(), getClass().getSimpleName(), R.id.nav_host_fragment_menu);
                         }
                     }
                     break;
-                case  R.id.bottomnav_info:
-                    try{
+                case R.id.bottomnav_info:
+                    try {
                         NavHostFragment.findNavController(FragmentManager.findFragment(view)).navigate(R.id.action_boulderViewFragment_to_infoBoulderViewFragment);
-                    } catch (IllegalStateException is){
+                    } catch (IllegalStateException is) {
                         if (getActivity() != null) {
-                            Utils.insertFragment((AppCompatActivity) getActivity(),new InfoBoulderViewFragment(),getClass().getSimpleName(),R.id.nav_host_fragment_menu);
+                            Utils.insertFragment((AppCompatActivity) getActivity(), new InfoBoulderViewFragment(), getClass().getSimpleName(), R.id.nav_host_fragment_menu);
                         }
                     }
                     break;
@@ -109,45 +119,85 @@ public class BoulderViewFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if(!mIsCreated){
+        if (!mIsCreated) {
             mIsCreated = true;
             requestImmage();
         } else {
-            if(getView()!=null){
-                ((ImageView)getView().findViewById(R.id.boulder_imageview)).setImageBitmap(model.getBitmap().getValue());
+            if (getView() != null) {
+                ((ImageView) getView().findViewById(R.id.boulder_imageview)).setImageBitmap(model.getBitmap().getValue());
             }
         }
     }
 
-    private void requestImmage(){
-        if(internetManager.isNetworkConnected()){
-            String imgName;
-            if (model.getSelected().getValue() != null) {
-                imgName = model.getSelected().getValue().getImg();
-            } else{
-                imgName = null;
-            }
-            if(imgName != null){
-                JsonObjectRequest jsonRequest = new JsonObjectRequest(InternetManager.URL + "?img="+imgName,null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        byte[] decodedString = new byte[0];
-                        try {
-                            decodedString = Base64.decode(response.getString("img").getBytes(StandardCharsets.UTF_8),Base64.DEFAULT);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString,0, decodedString.length);
-                        if (getView() != null) {
-                            ((ImageView)getView().findViewById(R.id.boulder_imageview)).setImageBitmap(decodedByte);
-                        }
-                        model.setBitmap(decodedByte);
-                    }
-                }, Throwable::printStackTrace);
-                VolleySingleton.getInstance(getContext()).add(jsonRequest);
-            }
+    private void requestImmage() {
+        String imgName;
+        if (model.getSelected().getValue() != null) {
+            imgName = model.getSelected().getValue().getImg();
         } else {
-            internetManager.getSnackbar().show();
+            imgName = null;
         }
+        if (imgName != null) {
+            String[] files = getContext().fileList();
+            List filesList = Arrays.asList(files);
+            if (filesList.contains(imgName)) {
+                String buffer = "";
+                try {
+                    FileInputStream fIn = new FileInputStream( new File(this.getContext().getFilesDir(),imgName));
+                    InputStreamReader reader = new InputStreamReader(fIn);
+                    BufferedReader bReader = new BufferedReader(reader);
+                    String dataRow = "";
+                    while ((dataRow = bReader.readLine()) != null) {
+                        buffer += dataRow;
+                    }
+                    bReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                setBitmap(buffer);
+            } else {
+                if (internetManager.isNetworkConnected()) {
+                    volleyRequestForImage(imgName);
+                } else {
+                    internetManager.getSnackbar().show();
+                }
+            }
+        }
+    }
+
+
+    private void volleyRequestForImage(String imgName) {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(InternetManager.URL + "?img=" + imgName, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                byte[] decodedString = new byte[0];
+                String imgData = null;
+                try {
+                    imgData = response.getString("img");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                setBitmap(imgData);
+                createImgFile(imgName, imgData);
+            }
+        }, Throwable::printStackTrace);
+        VolleySingleton.getInstance(getContext()).add(jsonRequest);
+    }
+
+    private void createImgFile(String imgName, String imgData) {
+        File file = new File(getContext().getFilesDir(), imgName);
+        try (FileOutputStream fos = getContext().openFileOutput(imgName, Context.MODE_PRIVATE)) {
+            fos.write(imgData.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setBitmap(String data){
+        byte[] decodedString = Base64.decode(data.getBytes(StandardCharsets.UTF_8), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        if (getView() != null) {
+            ((ImageView) getView().findViewById(R.id.boulder_imageview)).setImageBitmap(decodedByte);
+        }
+        model.setBitmap(decodedByte);
     }
 }
